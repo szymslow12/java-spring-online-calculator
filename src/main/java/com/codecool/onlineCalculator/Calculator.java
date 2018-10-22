819df4ee3250b33c785ff30a04b1830f89e647e8
szymslow12 <szym.slow12@gmail.com>
2018-10-22 20:45:06 +0200

    add/implement main in Calculator

---
+++ src/main/java/com/codecool/onlineCalculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.codecool.onlineCalculator;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Calculator.class, args);
+    }
+}
