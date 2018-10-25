2bc5ec8ea031c16d49868543fe310c5147e4dcd9
szymslow12 <szym.slow12@gmail.com>
2018-10-25 23:27:28 +0200

    change long to double in inputed numbers, add String[] actions, action, result, setFirst/SecondNumber, getFirst/SecondNumber, setAction, getAction, result, getResult, getActions, setActions

--- src/main/java/com/codecool/onlineCalculator/models/Calculator.java
+++ src/main/java/com/codecool/onlineCalculator/models/Calculator.java
@@ -2,37 +2,69 @@ package com.codecool.onlineCalculator.models;
 
 public class Calculator {
 
-    private long firstNumber;
-    private long secondNumber;
-    private String action;
+    private double firstNumber;
+    private double secondNumber;
+    private String[] actions = new String[] {"+", "-", "*", "\\\\"};
+    private Action action;
+    private double result;
 
 
-    public void setFirstNumber(long firstNumber) {
+    public void setFirstNumber(double firstNumber) {
         this.firstNumber = firstNumber;
     }
 
 
-    public long getFirstNumber() {
+    public double getFirstNumber() {
         return firstNumber;
     }
 
 
-    public void setSecondNumber(long secondNumber) {
+    public void setSecondNumber(double secondNumber) {
         this.secondNumber = secondNumber;
     }
 
 
-    public long getSecondNumber() {
+    public double getSecondNumber() {
         return secondNumber;
     }
 
 
     public void setAction(String action) {
-        this.action = action;
+        this.action = new ActionFactory().calculateByAction(action);
     }
 
 
     public String getAction() {
-        return action;
+        return action != null ? action.toString(): "null";
+    }
+
+
+    public double result() {
+        result = action != null ? action.result(firstNumber, secondNumber): 0.0;
+        return result;
+    }
+
+
+    public double getResult() {
+        return result();
+    }
+
+    /*public String getActions() {
+        StringBuilder jsonArray = new StringBuilder("[");
+        for (int i = 0; i < actions.length; i++) {
+            jsonArray.append(String.format("\"%s\",", actions[i]));
+//            jsonArray.append(String.format("\"%s\"=\"%s\",", "number" + i, actions[i]));
+        }
+        jsonArray.deleteCharAt(jsonArray.lastIndexOf(","));
+        jsonArray.append("]");
+        return jsonArray.toString();
+    }*/
+    public String[] getActions() {
+        return this.actions;
+    }
+
+
+    public void setActions(String[] actions) {
+        this.actions = actions;
     }
 }
