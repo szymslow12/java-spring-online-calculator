625afb8bf8987b9f16b9a889a0432ad503a843f8
szymslow12 <szym.slow12@gmail.com>
2018-10-25 23:24:21 +0200

    add onlineCalculator, collectConfiguration

--- src/main/java/com/codecool/onlineCalculator/controllers/CalculatorController.java
+++ src/main/java/com/codecool/onlineCalculator/controllers/CalculatorController.java
@@ -1,13 +1,26 @@
 package com.codecool.onlineCalculator.controllers;
 
+import com.codecool.onlineCalculator.models.Calculator;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class CalculatorController {
 
     @GetMapping("/index")
-    public String onlineCalculator() {
+    public String onlineCalculator(Model model) {
+        model.addAttribute("calculator", new Calculator());
+        return "index";
+    }
+
+
+    @PostMapping("/index")
+    public String collectConfiguration(@ModelAttribute Calculator calculator) {
         return "index";
     }
 }
