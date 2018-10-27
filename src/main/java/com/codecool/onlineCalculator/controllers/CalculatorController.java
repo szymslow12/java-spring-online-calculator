b94bb7a7bd483311ed4822f94cd3a9991bf6a6fc
szymslow12 <szym.slow12@gmail.com>
2018-10-27 14:24:32 +0200

    remove unused import

--- src/main/java/com/codecool/onlineCalculator/controllers/CalculatorController.java
+++ src/main/java/com/codecool/onlineCalculator/controllers/CalculatorController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class CalculatorController {
 
